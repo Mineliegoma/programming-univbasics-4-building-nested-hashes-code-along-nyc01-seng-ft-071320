95a656c0a42b7cbc1e1e95f12ed69a322376a11c
adept-bell-4492 <garlandegoma@gmail.com>
2020-07-05 21:57:22 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,11 +1,13 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriach=>{}, :matriarch=>{}, :hero=>{}, :hero_friend=>{
+    :montague => {:patriach=>{}, :matriarch=>{}, :hero=>{}, :hero_friend=>{}
       #your key/value pairs here!
       }, 
     :capulet => {
       #your key/value pairs here!
-      }}
+      }
+    
+  }
 
   
 end
